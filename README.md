35ba9dfaac39366e014f5a8f5071174ba149cf91
nurulina02 <129194113+nurulina02@users.noreply.github.com>
2023-04-12 05:37:43 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# Quiz App - Flutter Basics
+
+Flutter Basics - Quiz App
+
+## Stater Cleaned App
+https://gist.github.com/doozie-akshay/a1bf400170a90ab4f70d7eaf0d177bc9
+
+## Reference
+
+This project is a starting point for a Flutter Basics.
+Refer Video tutorials : https://www.youtube.com/playlist?list=PLEhZFQ3TRAyMubouRQtJmCGoa4H8XJaQH
---
+++ main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/quiz.dart';
+import 'package:quiz_app/result.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1}
+      ]
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9}
+      ]
+    },
+    {
+      'questionText': 'Whos is your favorite instructor?',
+      'answers': [
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1}
+      ]
+    },
+  ];
+  var _questionIndex = 0;
+  var _totalScore = 0;
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    setState(() {
+      _questionIndex += 1;
+      _totalScore += score;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quiz App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questions: _questions,
+                questionIndex: _questionIndex)
+            : Result(_totalScore, _resetQuiz),
+      ),
+    );
+  }
+}
